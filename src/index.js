464867733bde39b04f655b36dbb169cde4816920
Adrien Rousselet <adrien.rousselet@thetribe.io>
2020-12-16 09:58:07 +0100

    add MaterialUI components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 
 import './styles/styles.scss';
 
-function App() {
-    const [state, setState] = useState("CLICK ME");
+const rows = [
+    {person: 'Me', age: 35},
+    {person: 'Myself', age: 35},
+    {person: 'I', age: 30},
+];
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+
+function App() {
+    return (
+        <>
+            <Button variant="contained" color="primary">
+                I'm a Material UI Button
+            </Button>
+            <TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Person</TableCell>
+                            <TableCell align="right">Age</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.person}>
+                            <TableCell>{row.person}</TableCell>
+                            <TableCell align="right">{row.age}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
 }
 
 render(<App />, document.getElementById("root"));
